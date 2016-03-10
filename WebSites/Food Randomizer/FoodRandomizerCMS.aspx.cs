f3f8d18b9187e9867ebe9afdfcdaa205a1fe9213
unknown <jessejongsma@hotmail.com>
2016-03-10 13:51:13 +0100

    lekker

---
+++ WebSites/Food Randomizer/FoodRandomizerCMS.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Odbc;
+using System.Configuration;
+
+public partial class FoodRandomizerCMS : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            using (OdbcConnection connection = new OdbcConnection(ConfigurationManager.ConnectionStrings["MySQLConnStr"].ConnectionString))
+            {
+                connection.Open();
+                using (OdbcCommand command = new OdbcCommand("SELECT user FROM user", connection))
+                using (OdbcDataReader dr = command.ExecuteReader())
+                {
+                    while (dr.Read())
+                        Response.Write(dr["user"].ToString() + "<br />");
+                    dr.Close();
+                }
+                connection.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Response.Write("An error occured: " + ex.Message);
+        }
+    }
+}
---
+++ WebSites/Food Randomizer/Home.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Home : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        label.Visible = false;
+    }
+    public void randomNumber(object sender, EventArgs e) 
+    {
+        Random rnd = new Random();
+        int random = rnd.Next(0, 100);
+        string randomString = Convert.ToString(random);
+        label.Text = randomString;
+        label.Visible = true;
+    }
+}
